bff31db992193b57a05a0c65e1cd9d0c4a2454cf
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-09-04 22:44:00 +0100

    Compleed exercise 1 which was to swap variables

---
+++ exercise.js
@@ -0,0 +1,13 @@
+// Exercise 1
+// Write a program to swap the value of two variables, a and b. that is, a = blue
+
+let a = "red";
+let b = "blue";
+
+let c = a;
+
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
