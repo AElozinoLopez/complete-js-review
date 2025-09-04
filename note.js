cc8881871ef57f8ef181f83d52c9909b870d2918
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-09-04 23:32:41 +0100

    Used if...else statement to display hour of day

--- note.js
+++ note.js
@@ -149,12 +149,9 @@ console.log(currentColor);
 // If hour is between 12pm and 6pm: Good afternoon
 // Otherwise: Good evening
 
-if (condition) {
-  statement;
-} else if (condition) {
-  statement;
-} else if (anotherCondition) {
-  statement;
-} else if (yetAnotherCondition) {
-  statement;
-} else statement;
+let hour = 10;
+if (hour >= 6 && hour < 12) {
+  console.log("Good morning");
+} else if (hour >= 12 && hour < 18) {
+  console.log("Good afternoon");
+} else console.log("Good evening");
