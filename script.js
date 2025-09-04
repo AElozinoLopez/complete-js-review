8a16a4f35881ad3b6c3a36ad3e1a1376bfb5f1c2
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-09-04 16:38:51 +0100

    Declared an object and used the dot and bracket notation to update the values

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Welcome back to this review!");
+
+let name = "Elozino Lopez";
+// console.log(name);
+
+// Objects
+let person = {
+  name: "Elozino Lopez",
+  age: 30,
+  city: "New York",
+};
+
+// Dot notation to update the value
+person.name = "ASAIJE";
+
+//Bracket notation to update the value
+let selected = "name";
+person[selected] = "ELopez";
+console.log(person.name);
