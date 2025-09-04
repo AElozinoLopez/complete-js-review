7a12fa6d7a32f86b23a743dde053bfde025fa612
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-09-04 16:54:44 +0100

    Declared an an array and printed the 2nd item to console for test purposes

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let name = "Elozino Lopez";
 // console.log(name);
 
 // Objects
+// To group related data together
+
 let person = {
   name: "Elozino Lopez",
   age: 30,
@@ -16,4 +18,11 @@ person.name = "ASAIJE";
 //Bracket notation to update the value
 let selected = "name";
 person[selected] = "ELopez";
+
 console.log(person.name);
+
+// Arrays
+// To group related data together in a list
+
+let selectedColors = ["red", "blue"];
+console.log(selectedColors[1]);
