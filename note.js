6c5cac316300b0d8d7909c7272d6f0aa32ce3c9a
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-09-05 09:10:20 +0100

    Wrote a for loop to get odd numbers

--- note.js
+++ note.js
@@ -180,3 +180,8 @@ else console.log("Unknown");
 // for (initialExpression; Condition; increment) statement
 for (i = 0; i < 5; i++) console.log("For loop", i);
 for (i = 1; i <= 5; i++) console.log("The For loop", i);
+
+// To get odd numbers
+for (i = 1; i <= 5; i++) {
+  if (i % 2 !== 0) console.log(i);
+}
