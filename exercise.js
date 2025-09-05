28d56e3c38506a91f305a9fe2428cc32a971af48
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-09-05 19:36:25 +0100

    Executed exercise 3 which is to write a function to determine landscape or portrait

--- exercise.js
+++ exercise.js
@@ -13,11 +13,49 @@ console.log(a);
 console.log(b);
 
 // Exercise 2
-// Write a function that takes two numbers and returns the maximum of the two
+// Write a function that takes two numbers and returns the maximum of the two numbers
 
+// Solution 1 <clumsy though>
+// function max(a, b) {
+//   if (a > b) console.log("The maximum number is " + a);
+//   else if (a === b) console.log("The numbers are the same");
+//   else console.log("The maximum number is " + b);
+// }
+// max(7, 12);
+
+// Soluiton 2
+// function max(a, b) {
+//   if (a > b) return a;
+//   // else if (a === b) console.log("The numbers are the same");
+//   else return b;
+// }
+// let number = max(7, 12);
+// console.log(number);
+
+// // Soluiton 3
+// function max(a, b) {
+//   if (a > b) return a;
+//   // else if (a === b) console.log("The numbers are the same");
+//   return b;
+// }
+// let number = max(7, 12);
+// console.log(number);
+
+// Soluiton 4 and the best. using ternary operator
 function max(a, b) {
-  if (a > b) console.log("The maximum number is " + a);
-  else if (a === b) console.log("The numbers are the same");
-  else console.log("The maximum number is " + b);
+  return a > b ? a : b;
+  // else if (a === b) console.log("The numbers are the same");
 }
-max(7, 12);
+let number = max(7, 12);
+console.log(number);
+
+// Exercise 3
+// Write a function that takes two parameters, width and height and returns true if
+// the image islandscape and false if portrait
+
+function isLandscape(width, height) {
+  // return (width > height) ? true : false;
+  return width > height;
+}
+
+console.log("Picture is landscape: " + isLandscape(1200, 7800));
