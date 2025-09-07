917712f004fb937593bfb84b214998ae99aa359e
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-09-07 05:34:04 +0100

    Reduced the parameters of the constructor function

--- exercise.js
+++ exercise.js
@@ -169,19 +169,12 @@ for (content in blog) console.log(content, blog[content]);
 
 // Exercise 4
 // Create a constructor function called post for a blog yet to be published
-function Post(title, author, views, comment, isLive) {
+function Post(title, author, views) {
   (this.title = "title"),
     (this.author = "author"),
     (this.views = 10000000),
-    (this.comment = [
-      {
-        title: title,
-        author: author,
-      },
-    ]),
-    (this.draw = function () {
-      return isLive;
-    });
+    (this.comment = []),
+    (this.isLive = false);
 }
 
 const blogPost = new Post("a", "b", 10, "c", "false");
