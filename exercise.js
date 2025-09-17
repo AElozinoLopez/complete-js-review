1f3d83ddce3a87268ec60c95de8bd3736dee521a
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-09-17 03:46:07 +0100

    Used the reduce method to return the number of times of occurrence in the countOccurrence funciton

--- exercise.js
+++ exercise.js
@@ -267,9 +267,16 @@ function move(array, index, offset) {
 const numbersEx5 = [1, 2, 3, 4, 1];
 
 function countOccurrences(array, searchElement) {
-  let count = 0;
-  for (let element of array) if (element === searchElement) count++;
-  return count;
+  // Solution 1
+  // let count = 0;
+  // for (let element of array) if (element === searchElement) count++;
+  // return count;
+
+  // Solution 2
+  array.reduce((accumulator, current) => {
+    const occurrence = current === searchElement ? 1 : 0;
+    return accumulator + occurrence;
+  }, 0);
 }
 
 const count = countOccurrences(numbersEx5, 1);
