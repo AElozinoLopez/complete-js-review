a8e42bdb7f1220721a7ad3b44f96a8d27bd63632
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-10-06 09:04:33 +0100

    Applied some methods to the movies object

--- exercise.js
+++ exercise.js
@@ -323,4 +323,12 @@ const movies = [
 ];
 
 // Solution
-function movieRanking(movies) {}
+// function movieRanking(movies) {
+//   const movieYear =
+// }
+
+movies
+  .filter((m) => m.year === 2025 && m.rating >= 4) // To get the year and rating
+  .sort((a, b) => a.rating - b.rating) // Similar to saying
+  .reverse() // To reverse the output of .sort to descending order
+  .map((m) => m.title); // To print only the tile of .reverse output
